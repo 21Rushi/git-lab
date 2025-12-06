c1f7a8e14098db887140e3f0ae9609152cd502f9
Rushikesh Dhule <89271423+21Rushi@users.noreply.github.com>
2025-12-06 21:41:51 +0530

    Delete Todo_app/BackEnd directory

--- Todo_app/BackEnd/app.js
+++
@@ -1,8 +0,0 @@
-app.post("/submittodoitem", async (req, res) => {
-  const { itemName, itemDescription } = req.body;
-
-  await Todo.create({ itemName, itemDescription });
-
-  res.json({ message: "Item Saved Successfully" });
-});
-
